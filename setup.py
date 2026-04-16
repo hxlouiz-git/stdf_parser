7ce9d3d89e7a0e11f21ff5dbc9966e5c74194072
Delos reyes <Haniel.Delosreyes@analog.com>
2026-04-16 18:46:28 +0800

    fixed install issue

--- setup.py
+++ setup.py
@@ -3,7 +3,19 @@ import os
 from setuptools import setup, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.command.install_lib import install_lib
-from Cython.Build import cythonize
+
+# Try to import Cython, but make it optional
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
+    print("Cython not available, installing without compilation")
+
+# Check environment variable to disable Cython
+if os.environ.get('STDF_PARSER_NO_CYTHON', '').lower() in ('1', 'true', 'yes'):
+    USE_CYTHON = False
+    print("Cython disabled via STDF_PARSER_NO_CYTHON environment variable")
 
 # All .py source files to compile except __init__.py (which only has imports)
 source_files = [
@@ -14,7 +26,7 @@ source_files = [
 # Module names that will have compiled .pyd counterparts
 compiled_modules = {
     os.path.splitext(os.path.basename(f))[0] for f in source_files
-}
+} if USE_CYTHON else set()
 
 
 class BuildPyExcludeSource(build_py):
@@ -37,18 +49,22 @@ class InstallLibExcludeC(install_lib):
         return outfiles
 
 
-setup(
-    packages=find_packages(),
-    ext_modules=cythonize(
+setup_kwargs = {
+    "packages": find_packages(),
+}
+
+if USE_CYTHON:
+    setup_kwargs["ext_modules"] = cythonize(
         source_files,
         build_dir="build_cython",   # .c files go here, not into stdf_parser/
         compiler_directives={"language_level": "3"},
         annotate=False,
-    ),
-    cmdclass={
+    )
+    setup_kwargs["cmdclass"] = {
         "build_py": BuildPyExcludeSource,
         "install_lib": InstallLibExcludeC,
-    },
-    package_data={"stdf_parser": ["*.pyd"]},
-    exclude_package_data={"stdf_parser": ["*.c", "*.py"]},
-)
+    }
+    setup_kwargs["package_data"] = {"stdf_parser": ["*.pyd"]}
+    setup_kwargs["exclude_package_data"] = {"stdf_parser": ["*.c", "*.py"]}
+
+setup(**setup_kwargs)
