0fc252249021d7036d957cc5ce416cacb10c6d6b
Delos reyes <Haniel.Delosreyes@analog.com>
2026-04-16 18:36:41 +0800

    comment out __init__.py on stdf_parser

--- stdf_parser/__init__.py
+++ stdf_parser/__init__.py
@@ -1,5 +1,5 @@
-from .ByteFuncs import *
-from .STDFWriter import *
-from .RECRecipes import *
-from .RECFuncs import *
-from .RecordTuples import *
+# from .ByteFuncs import *
+# from .STDFWriter import *
+# from .RECRecipes import *
+# from .RECFuncs import *
+# from .RecordTuples import *
